67abf517c011eaed701484ffd0be403ee4eb7891
miracool45 <aokeosisi@gmail.com>
2022-09-08 16:58:30 -0700

    task 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistdio.h>
+
+/**
+ * main - Entry point
+ * Return:Always 1
+ */              
+
+int main(void)
+{
+	write(2, "and that peice of art is useful\" - Dora korparv, 2015-20-19\n",59");
+      	return (1);
+}
