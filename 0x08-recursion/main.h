0b546fabb79a1fb416390cebd0716dc581668dfa
miracool45 <aokeosisi@gmail.com>
2022-09-28 09:10:31 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-void -putcha(char c);
+void _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -6,4 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
